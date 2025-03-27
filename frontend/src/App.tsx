d9df7a5c6af2678cd1b538c975db13befae592a9
ishuvam009 <120933772+ishuvam009@users.noreply.github.com>
2025-03-27 23:19:24 +0530

    Commit

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,13 @@
 import "./App.css";
 import { Header } from "./components/Header";
+import { FaGithub } from "react-icons/fa";
 
 function App() {
   return (
     <>
       <div className="mx-60 mt-16 font-mono h-screen">
         <Header />
+        {/* //About Section */}
         <div className="mt-20 mx-10">
           <p className="text-2xl font-semibold mb-6">Hey there !</p>
           <p>
@@ -29,27 +31,81 @@ function App() {
           </p>
           <div className="mt-6">
             <p>Find me on </p>
+            <FaGithub />
           </div>
         </div>
+        {/* //Tech Stack Section */}
         <div className="mt-15 mx-10">
-          <p className="font-semibold text-xl mb-6">Technologies I have worked with</p>
+          <p className="font-semibold text-xl mb-6">
+            Technologies I have worked with
+          </p>
           <div className="flex flex-col gap-y-4">
-          <p><span className="underline">Programming Languages</span> : Javascript / Typescript, Python, C++.</p>
-          <p><span className="underline">Frontend</span> : ReactJS, NextJS, TailwindCSS.</p>
-          <p><span className="underline">Backend</span> : NodeJS, ExpressJS, FastAPI, Hono, ZOD.</p>
-          <p><span className="underline">Database</span> : Postgre, SQL, MongoDB, Prisma, Redis.</p>
-          <p><span className="underline">Devops</span> : AWS, Cloudflare, NGINX, Docker, </p>
+            <p>
+              <span className="underline">Programming Languages</span> :
+              Javascript / Typescript, Python, C++.
+            </p>
+            <p>
+              <span className="underline">Frontend</span> : ReactJS, NextJS,
+              TailwindCSS.
+            </p>
+            <p>
+              <span className="underline">Backend</span> : NodeJS, ExpressJS,
+              FastAPI, Hono, ZOD.
+            </p>
+            <p>
+              <span className="underline">Database</span> : Postgre, SQL,
+              MongoDB, Prisma, Redis.
+            </p>
+            <p>
+              <span className="underline">Devops</span> : AWS, Cloudflare,
+              NGINX, Docker,{" "}
+            </p>
           </div>
         </div>
+        {/* //Experience Section */}
         <div className="mt-16 mx-10">
           <p className="font-semibold text-xl mb-6">Experience</p>
           <div>
-            <p>May 2024 - Sep 2024 <span className="font-bold">Open Source Contributor</span> @GSSOC</p>
+            <p>
+              <span className="text-slate-600">May 2024 - Sep 2024</span>{" "}
+              <span className="font-bold">Open Source Contributor</span> @GSSOC
+            </p>
           </div>
         </div>
+        {/* //Projects Section */}
         <div className="mt-16 mx-10">
           <p className="font-semibold text-xl mb-6">Projects</p>
-          <div></div>
+          <div>
+            <div className="mb-4">
+              <p className="underline underline-offset-4">
+                FinSight AI - AI-Powered Financial Insights Engine.
+              </p>
+              <p className="text-orange-400">
+                [React, FastAPI, Langchain, AWS Lambda, FAISS]
+              </p>
+            </div>
+            <div className="mb-4">
+              <p className="underline underline-offset-4">
+                ScribeX - Medium like blogging platform.
+              </p>
+              <p className="text-orange-400">
+                [Hono, Reacct, Postgresql, Cloudflare Workers, Prisma, ZOD,
+                Connection Pooling]
+              </p>
+            </div>
+            <div className="mb-4">
+              <p className="underline underline-offset-4">
+                Payment Wallet - Peer to peer money transfer,
+              </p>
+              <p className="text-orange-400">
+                [React, Express.js, Node.js, MongoDB, Vercel, JWT, ZOD]
+              </p>
+            </div>
+          </div>
+        </div>
+        {/* //Footer Section */}
+        <div className="mt-30 flex justify-center">
+          <p>© SHUVAM MANDAL</p>
         </div>
       </div>
     </>
