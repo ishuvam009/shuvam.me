80fad121a1b2882524e87fba1de66c74610bead5
ishuvam <ishuvam549@gmail.com>
2025-04-27 00:02:38 +0530

    Commit

--- frontend/src/components/ProjectComp.tsx
+++ frontend/src/components/ProjectComp.tsx
@@ -6,37 +6,41 @@ export const ProjectComp = () => {
       <div className="mt-16 mx-10">
         <p className="font-semibold text-xl mb-6">Projects</p>
         <div>
-          <div className="mb-4">
-            <Link to={""}>
-              <p className="underline underline-offset-4">
-                FinSight AI - AI-Powered Financial Insights Engine.
+          <Link to={"/projects/finsight"}>
+            <div className="mb-4">
+                <p className="underline underline-offset-4">
+                  FinSight AI - AI-Powered Financial Insights Engine.
+                </p>
+              <p className="text-orange-400">
+                [React, FastAPI, Langchain, AWS Lambda, FAISS]
               </p>
-            </Link>
-            <p className="text-orange-400">
-              [React, FastAPI, Langchain, AWS Lambda, FAISS]
-            </p>
-          </div>
-          <div className="mb-4">
-            <Link to={""}>
-              <p className="underline underline-offset-4">
-                ScribeX - Medium like blogging platform.
+            </div>
+          </Link>
+          <Link to={""}>
+            <div className="mb-4">
+              <Link to={""}>
+                <p className="underline underline-offset-4">
+                  ScribeX - Medium like blogging platform.
+                </p>
+              </Link>
+              <p className="text-orange-400">
+                [Hono, Reacct, Postgresql, Cloudflare Workers, Prisma, ZOD,
+                Connection Pooling]
               </p>
-            </Link>
-            <p className="text-orange-400">
-              [Hono, Reacct, Postgresql, Cloudflare Workers, Prisma, ZOD,
-              Connection Pooling]
-            </p>
-          </div>
-          <div className="mb-4">
-            <Link to={""}>
-              <p className="underline underline-offset-4">
-                Payment Wallet - Peer to peer money transfer,
+            </div>
+          </Link>
+          <Link to={""}>
+            <div className="mb-4">
+              <Link to={""}>
+                <p className="underline underline-offset-4">
+                  Payment Wallet - Peer to peer money transfer,
+                </p>
+              </Link>
+              <p className="text-orange-400">
+                [React, Express.js, Node.js, MongoDB, Vercel, JWT, ZOD]
               </p>
-            </Link>
-            <p className="text-orange-400">
-              [React, Express.js, Node.js, MongoDB, Vercel, JWT, ZOD]
-            </p>
-          </div>
+            </div>
+          </Link>
         </div>
       </div>
     </>
