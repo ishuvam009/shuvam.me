b7afaa7ebdd11b8517a6b5da9b74e7baeed913e4
ishuvam <ishuvam549@gmail.com>
2025-05-04 01:41:26 +0530

    Commit

--- frontend/src/pages/ProjectOne.tsx
+++ frontend/src/pages/ProjectOne.tsx
@@ -1,7 +1,17 @@
+import { Header } from "../components/Header"
+import image1 from "../assets/FINSIGHT.png"
+
 export const ProjectOne = () => {
     return(
         <>
-        <p>Hello fromp roject one.</p>
+            <div className="px-60 pt-16">
+                <Header />
+                <div className="mt-20">
+                    <p className="text-xl mb-10 font-semibold">AI - powered finance chat system capable of processing and retrieving information from
+                    financial articles to provide intelligent responses.</p>
+                    <img src={image1} alt="" />
+                </div>
+            </div>
         </>
     )
 }
