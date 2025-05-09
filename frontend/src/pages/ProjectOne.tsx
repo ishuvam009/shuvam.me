6ba523cafbe39cd13c5bbe537e5794c6dcafa959
ishuvam <ishuvam549@gmail.com>
2025-05-09 21:59:04 +0530

    Commit

--- frontend/src/pages/ProjectOne.tsx
+++ frontend/src/pages/ProjectOne.tsx
@@ -10,7 +10,24 @@ export const ProjectOne = () => {
                     <p className="text-xl mb-10 font-semibold">AI - powered finance chat system capable of processing and retrieving information from
                     financial articles to provide intelligent responses.</p>
                     <img src={image1} alt="finsight" />
-                    
+                </div>
+                <div className="mt-20">
+                    <p className="text-2xl font-semibold">Tech Stack</p>
+                    <ul className="list-disc mt-5 ml-8">
+                        <li>VectorDB</li>
+                        <li>LLM</li>
+                        <li>RAG</li>
+                        <li>FastAPI</li>
+                        <li>React</li>
+                        <li>MongoDB</li>
+                    </ul>
+                </div>
+
+                <div className="mt-10">
+                    <p className="text-xl font-semibold">Features Build</p>
+                    <ul className="list-disc mt-5 ml-8">
+                        <li></li>
+                    </ul>
                 </div>
             </div>
         </>
